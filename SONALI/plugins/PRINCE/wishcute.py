96f5a8de3ba4aa1b4e10afbd501c220663f4265c
‼ 𝐏ʀɪɴᴄᴇ 𖤍 𝐂ᴏᴅᴇ𝐙 𒁍 <priyanshsinff@gmail.com>
2025-03-27 19:32:18 +0530

    Update wishcute.py

--- SONALI/plugins/PRINCE/wishcute.py
+++ SONALI/plugins/PRINCE/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SONALI import app 
 
-SUPPORT_CHAT = "krishnetwork"
+SUPPORT_CHAT = "SWEETY_BOT_UPDATE"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
