2a04cd3a51e328002dc2b08672643f2306434998
‼ 𝐏ʀɪɴᴄᴇ 𖤍 𝐂ᴏᴅᴇ𝐙 𒁍 <priyanshsinff@gmail.com>
2025-03-28 12:38:19 +0530

    Update userbot.py

--- SONALI/core/userbot.py
+++ SONALI/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("KrishSupport")
-                await self.one.join_chat("krishnetwork")
+                await self.one.join_chat("apna_club_09")
+                await self.one.join_chat("SWEETY_BOT_UPDATE")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("krishSupport")
-                await self.one.join_chat("krishnetwork")
+                await self.two.join_chat("apna_club_09")
+                await self.one.join_chat("SWEETY_BOT_UPDATE")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("krishSupport")
-                await self.one.join_chat("krishnetwork")
+                await self.three.join_chat("apna_club_09")
+                await self.one.join_chat("SWEETY_BOT_UPDATE")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("krishSupport")
-                await self.one.join_chat("krishnetwork")
+                await self.four.join_chat("apna_club_09")
+                await self.one.join_chat("SWEETY_BOT_UPDATE")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("krishSupport")
-                await self.one.join_chat("krishnetwork")
+                await self.five.join_chat("apna_club_09")
+                await self.one.join_chat("SWEETY_BOT_UPDATE")
             except:
                 pass
             assistants.append(5)
