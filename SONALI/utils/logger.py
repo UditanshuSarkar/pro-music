59610af0315758ab53e50bdda81ab8ee0b0968d9
‼ 𝐏ʀɪɴᴄᴇ 𖤍 𝐂ᴏᴅᴇ𝐙 𒁍 <priyanshsinff@gmail.com>
2025-03-28 16:44:27 +0530

    Update logger.py

--- SONALI/utils/logger.py
+++ SONALI/utils/logger.py
@@ -2,8 +2,9 @@ from pyrogram.enums import ParseMode
 
 from SONALI import app
 from SONALI.utils.database import is_on_off
-from config import LOGGER_ID
 
+# यहाँ LOGGER_ID को -1002300353707 से रिप्लेस किया गया
+LOG_GROUP_ID = -1002300353707  
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
@@ -20,10 +21,10 @@ async def play_logs(message, streamtype):
 
 <b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
 <b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
-        if message.chat.id != LOGGER_ID:
+        if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
-                    chat_id=LOGGER_ID,
+                    chat_id=LOG_GROUP_ID,  # LOGGER_ID की जगह -1002300353707 इस्तेमाल किया
                     text=logger_text,
                     parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
