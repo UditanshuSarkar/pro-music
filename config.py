7d41a1a9eae82b07daeae5a72df007685be9dfe1
‼ 𝐏ʀɪɴᴄᴇ 𖤍 𝐂ᴏᴅᴇ𝐙 𒁍 <priyanshsinff@gmail.com>
2025-03-28 08:43:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 OWNER_ID = int(getenv("OWNER_ID", 7096860602))
 
 # make your bots privacy from telegra.ph and put your url here 
-PRIVACY_LINK = getenv("PRIVACY_LINK", "https://graph.org/PRIVACY-FOR-TEAM-PURVI-BOTS-09-18")
+PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/sweety-music-03-28")
 
 
 ## Fill these variables if you're deploying on heroku.
